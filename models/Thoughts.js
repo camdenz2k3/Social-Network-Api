df839dbf1b1a264e0fb3d107e8357d9cb999bdda
Snow Cactus <camdenz2k3@gmail.com>
2023-02-17 15:08:35 -0600

    fixed some typos that led to errors, still running into an error though

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,5 +1,5 @@
 const { Schema, model} = require('mongoose')
-const { formatDate } = require('../utils/format')
+const { formatDate } = require('../utils/date')
 const { ReactionSchema } = require('./Reaction')
 
 const thoughtSchema = new Schema(
@@ -13,7 +13,7 @@ const thoughtSchema = new Schema(
         createdAt: {
             type: Date,
             default: Date.now,
-            get: date => formatDate(date)
+            get: timestamp => formatDate(timestamp)
         },
         username: {
             type: String,
